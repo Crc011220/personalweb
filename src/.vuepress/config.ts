6990d52cd31a7d7db8a8d844c928cda7e973a235
Ruochen Chen <ruocchen1220@gmail.com>
2024-11-06 23:01:22 +1100

    Nov 6, 2024 update

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -22,33 +22,33 @@ export default defineUserConfig({
   theme,
 
 
-  plugins: [
-    docsearchPlugin({
-      appId: "6264MA721Y",
-
-      apiKey: "006ddc432bcbe37fa6582bde28e2a386",
-
-      indexName: "crc011220io",
-      locales: {
-        '/': {
-          placeholder: 'Search Blogs',
-          translations: {
-            button: {
-              buttonText: 'Search Blogs',
-            },
-          },
-        },
-        '/zh/': {
-          placeholder: '搜索文档',
-          translations: {
-            button: {
-              buttonText: '搜索文档',
-            },
-          },
-        },
-      },
-    }),
-  ],
+  // plugins: [
+  //   docsearchPlugin({
+  //     appId: "6264MA721Y",
+  //
+  //     apiKey: "006ddc432bcbe37fa6582bde28e2a386",
+  //
+  //     indexName: "crc011220io",
+  //     locales: {
+  //       '/': {
+  //         placeholder: 'Search Blogs',
+  //         translations: {
+  //           button: {
+  //             buttonText: 'Search Blogs',
+  //           },
+  //         },
+  //       },
+  //       '/zh/': {
+  //         placeholder: '搜索文档',
+  //         translations: {
+  //           button: {
+  //             buttonText: '搜索文档',
+  //           },
+  //         },
+  //       },
+  //     },
+  //   }),
+  // ],
   // Enable it with pwa
   // shouldPrefetch: false,
 });
